917de174f001fd61dcaf51283e81dc90a2e67f14
58oks <144000876+58oks@users.noreply.github.com>
2023-09-13 17:36:53 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,39 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Transactional
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Transactional(readOnly = true)
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+   @Transactional
+   @Override
+   public User findByModel(String model) {
+      return userDao.findByModel(model);
+   }
+
+   @Transactional
+   @Override
+   public User findBySeries(int series) {
+      return userDao.findBySeries(series);
+   }
+}
